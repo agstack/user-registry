9597adbf6c1fb69d3175ad5d876657fdd41f7adb
Muhammad Hamza Ashraf <mhamza.141097@gmail.com>
2023-02-10 04:31:58 +0500

    Email and User activation.

--- utils_activation/email.py
+++ utils_activation/email.py
@@ -6,7 +6,7 @@ from dbms import app, mail
 def send_email(to, subject, template):
     msg = Message(
         subject,
-        recipients=["mhamza.141097@gmail.com"],
+        recipients=["your@email.com"],
         html=template,
         sender=app.config['MAIL_DEFAULT_SENDER']
     )
